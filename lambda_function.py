f306f2d7aeaf0bcf67a76a7adda7ff37f2195e6f
shoda888 <gekka888@gmail.com>
2020-05-02 13:24:48 +0900

    jsonを別ファイルで管理

--- lambda_function.py
+++ lambda_function.py
@@ -16,58 +16,12 @@ def lambda_handler(event, context):
     
     # 友達登録時
     if message_dist["events"][0]["type"] == "follow":
-        message = [
-          {
-            "type": "template",
-            "altText": "Thank you for subscribing!!",
-            "quickReply": {
-              "items": [
-                {
-                  "type": "action",
-                  "action": {
-                    "type": "camera",
-                    "label": "普通のCamera"
-                  }
-                }
-              ]
-            },
-            "template": {
-              "type": "buttons",
-              "actions": [
-                {
-                  "type": "postback",
-                  "label": "男",
-                  "displayText": "男",
-                  "data": "male"
-                },
-                {
-                  "type": "postback",
-                  "label": "女",
-                  "displayText": "女",
-                  "data": "female"
-                },
-                {
-                  "type": "postback",
-                  "label": "両方",
-                  "displayText": "両方",
-                  "data": "both"
-                },
-                {
-                  "type": "uri",
-                  "label": "(Debug)QRコードカメラ",
-                  "uri": "https://line.me/R/nv/QRCodeReader"
-                }
-              ],
-              "thumbnailImageUrl": "https://sho-d-blog.com/wp-content/uploads/2020/04/two_people_walking.jpg",
-              "title": "あなたの恋愛対象",
-              "text": "恋愛対象を教えて下さい",
-            }
-          }
-        ]
+        json_open = open('Romance.json', 'r')
+        json_load = json.load(json_open)
     
         params = {
             "replyToken": message_dist["events"][0]["replyToken"],
-            "messages": message
+            "messages": [json_load]
         }
         request = urllib.request.Request(url, json.dumps(params).encode("utf-8"), method=method, headers=headers)
         with urllib.request.urlopen(request) as res:
