ee72ab97c70e2fbbbedba802b4d3930a07c4e8e7
shoda888 <gekka888@gmail.com>
2020-05-02 14:53:00 +0900

    add Height Question

--- lambda_function.py
+++ lambda_function.py
@@ -16,15 +16,23 @@ def lambda_handler(event, context):
     
     # 友達登録時
     if message_dist["events"][0]["type"] == "follow":
-        json_open = open('Romance.json', 'r')
-        json_load = json.load(json_open)
-    
-        params = {
-            "replyToken": message_dist["events"][0]["replyToken"],
-            "messages": [json_load]
-        }
-        request = urllib.request.Request(url, json.dumps(params).encode("utf-8"), method=method, headers=headers)
-        with urllib.request.urlopen(request) as res:
-            body = res.read()
+        json_open = open('json/Romance.json', 'r')
+
+    elif message_dist["events"][0]["type"] == "postback":
+        if message_dist["events"][0]["postback"]["data"] == ("male" or "female" or "both"):
+            json_open = open('json/Height.json', 'r')
+
+    else:
+        json_open = open('json/error.json', 'r')
+
+    json_load = json.load(json_open)
+    params = {
+        "replyToken": message_dist["events"][0]["replyToken"],
+        "messages": [json_load]
+    }
+    request = urllib.request.Request(url, json.dumps(params).encode("utf-8"), method=method, headers=headers)
+    with urllib.request.urlopen(request) as res:
+        body = res.read()
+        print(body)
             
     return 0
