289435ca8ded48ad08345eb12afe7f8c287faa64
ren_higuchi <ren_higuchi@change-jp.com>
2020-05-03 21:57:40 +0900

    IAM情報をdafault使用するように変更

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "DEPLOY START!!"
 funcname="aisekiyaFunc"
 
 docker run --rm -v "$PWD":/var/task shoda888/local2pylambda:latest
-aws lambda update-function-code --function-name ${funcname} --zip-file fileb://deploy_package.zip --profile aisekiya-user
+aws lambda update-function-code --function-name ${funcname} --zip-file fileb://deploy_package.zip
