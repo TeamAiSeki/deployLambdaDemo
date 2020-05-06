dd7843ee36f1439fc8df36f1fb121a73a26dd9ed
ren_higuchi <ren_higuchi@change-jp.com>
2020-05-06 18:21:18 +0900

    一旦同期

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,27 @@
+"""後日、コード綺麗にします。ちょっと本業に想定の5倍くらい時間取られた"""
 import json
 import urllib.request
 import re
 import os
 import qrcode
-from PIL import Image
-import cv2
- 
-from datetime import datetime, timezone
+from PIL import ImageChops
+import re
+#各種機械学習モジュールのimport、使うなら
+# import keras
+# import numpy as np
+
+#User情報を引数に受け取り、診断結果を返す関数。オバソン記述予定。
+def matching(user_id):
+    #機械学習処理
+    model_output = "96%"
+    return model_output
  
 def lambda_handler(event, context):
+    read_flag = True
     url = "https://api.line.me/v2/bot/message/reply"
     method = "POST"
     headers = {
-        'Authorization': 'Bearer ' + os.environ['CHANNEL_ACCESS_TOKEN'],
+        'Authorization': 'Bearer ' + os.environ['DEV_CHANNEL_ACCESS_TOKEN'],
         'Content-Type': 'application/json'
     }
     message_dist = json.loads(event["body"])
@@ -44,28 +53,42 @@ def lambda_handler(event, context):
             json_open = open('json/Age.json', 'r')
         if postback_action == "age":
             json_open = open('json/Opinion.json', 'r')
+            print(json_open)
+            print(type(json_open))
         if postback_action == "opinion":
             json_open = open('json/Emotion.json', 'r')
         if postback_action == "emotion":
             # QRコードを生成しクエリパラメータとしてuser_idを与える、一旦正田氏のqrをそのまま利用する。
             # qr = qrcode.make('https://comingout.tokyo/1806'+ '/?user_id='+ user_id)
             json_open = open('json/Aicode.json', 'r')
-    
-    else:
-        json_open = open('json/error.json', 'r')
-    
-    # #ユーザーの画像送信をキャッチ
-    # if message_dist["events"][0]["type"] == "message":
-    #     if message_dist["events"][0]["message"]["type"] == "image":
             
     
-    #ユーザーのID送信をキャッチ
-    if message_dist["events"][0]["type"] == "message":
-        if message_dist["events"][0]["message"]["text"] == r"^[0-9]*$":
-            json_open = open('json/matching_result.json')
+    #ユーザーがBotに何らかを送信した場合
+    elif message_dist["events"][0]["type"] == "message":
+        # #ユーザーの画像送信をキャッチ
+        if message_dist["events"][0]["message"]["type"] == "image":
+            pass
+        #ユーザーのID送信をキャッチ
+        if re.match(r"^[0-9]{6}", message_dist["events"][0]["message"]["text"]) :
+            try :
+                json_load = {
+                    "type": "text",
+                    "text": "相性は"+ matching(user_id) + "です。すごーい"
+                    }
+                read_flag = False
+            except :
+                json_load = {
+                    "type": "text",
+                    "text": "相性診断に失敗しました。IDを確認してください。"
+                    }
+        else :
+            json_open = open('json/error.json', 'r')
+    else:
+        json_open = open('json/error.json', 'r')
 
-    json_load = json.load(json_open)
-    print("||||", json_load)
+    if read_flag :
+        json_load = json.load(json_open)
+
     params = {
         "replyToken": message_dist["events"][0]["replyToken"],
         "messages": [json_load]
