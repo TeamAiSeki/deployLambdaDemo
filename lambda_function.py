a942f1391a04a5a83b48275723b36537575eb99a
shoda888 <gekka888@gmail.com>
2020-05-17 11:48:00 +0900

    刷新した質問を適応

--- lambda_function.py
+++ lambda_function.py
@@ -19,30 +19,34 @@ def lambda_handler(event, context):
     if not message_dist["events"][0]["type"] == "unfollow":
         # 友達登録時
         if message_dist["events"][0]["type"] == "follow":
-            json_open = open('json/Romance.json', 'r')
+            json_open = open('json/Age.json', 'r')
 
         elif message_dist["events"][0]["type"] == "postback":
             postback = message_dist["events"][0]["postback"]["data"] 
             postback_action = postback.split("&")[0].split("=")[1]
             # DB保存用
             # postback_value = postback.split("&")[1].split("=")[1]
-            if postback_action == "romance":
-                json_open = open('json/Major.json', 'r')
-            if postback_action == "major":
-                json_open = open('json/Club.json', 'r')
-            if postback_action == "club":
-                json_open = open('json/Work.json', 'r')
-            if postback_action == "work":
+            if postback_action == "age":
+                json_open = open('json/Subject.json', 'r')
+            if postback_action == "subject":
+                json_open = open('json/Happy.json', 'r')
+            if postback_action == "happy":
+                json_open = open('json/Job.json', 'r')
+            if postback_action == "job":
                 json_open = open('json/Height.json', 'r')
             if postback_action == "height":
+                json_open = open('json/Want.json', 'r')
+            if postback_action == "want":
                 json_open = open('json/Style.json', 'r')
             if postback_action == "style":
-                json_open = open('json/Age.json', 'r')
-            if postback_action == "age":
-                json_open = open('json/Opinion.json', 'r')
-            if postback_action == "opinion":
-                json_open = open('json/Emotion.json', 'r')
-            if postback_action == "emotion":
+                json_open = open('json/Social1.json', 'r')
+            if postback_action == "social1":
+                json_open = open('json/Social3.json', 'r')
+            if postback_action == "social3":
+                json_open = open('json/Social2.json', 'r')
+            if postback_action == "social2":
+                json_open = open('json/Social4.json', 'r')
+            if postback_action == "social4":
                 json_open = open('json/Aicode.json', 'r')
 
         else:
